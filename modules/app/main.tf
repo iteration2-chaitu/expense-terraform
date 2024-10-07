493f4ee3d5069393a0fb7cab101b172eeaf69be6
chaitu <chaitus.ch09@gmail.com>
2024-10-07 15:35:46 -0700

    error correction

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -15,7 +15,7 @@ resource "aws_route53_record" "instance"{
   name = "terraform-${var.component}-${var.env}"
   type = "A"
   zone_id = var.zone_id
-  records = aws_instance.instance.private_ip
+  records = [aws_instance.instance.private_ip]
   ttl = 30
 }
 
