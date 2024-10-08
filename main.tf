970cd2cb9b16d9a3f962632aea7773ed11e09ce8
chaitu <chaitus.ch09@gmail.com>
2024-10-08 11:20:27 -0700

    backend and mysql module configuration adding in terraform

--- main.tf
+++ main.tf
@@ -7,24 +7,24 @@
    ssh_password            = var.ssh_password
     zone_id                 = var.zone_id
  }
-# module "backend" {
-#   source                  = "./modules/app"
-#   component                 =  "backend"
-#   env                     =  var.env
-#   instance_type           = var.instance_type
-#   ssh_user                = var.ssh_user
-#   ssh_password            = var.ssh_password
-#   zone_id                 = var.zone_id
-# }
-# module "mysql" {
-#   source                  = "./modules/app"
-#   component                 =  "mysql"
-#   env                     =  var.env
-#   instance_type           = var.instance_type
-#   ssh_user                = var.ssh_user
-#   ssh_password            = var.ssh_password
-#   zone_id                 = var.zone_id
-# }
+ module "backend" {
+   source                  = "./modules/app"
+   component                 =  "backend"
+   env                     =  var.env
+   instance_type           = var.instance_type
+   ssh_user                = var.ssh_user
+   ssh_password            = var.ssh_password
+   zone_id                 = var.zone_id
+ }
+ module "mysql" {
+   source                  = "./modules/app"
+   component                 =  "mysql"
+   env                     =  var.env
+   instance_type           = var.instance_type
+   ssh_user                = var.ssh_user
+   ssh_password            = var.ssh_password
+   zone_id                 = var.zone_id
+ }
 
 
 # module "backend" {
