9428ef394bd496528a192f077af5bee7ad558c40
chaitu <chaitus.ch09@gmail.com>
2024-10-08 13:35:49 -0700

    adding depedency in terraform

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@
     zone_id                 = var.zone_id
  }
  module "backend" {
+   depends_on         =[module.mysql]
    source                  = "./modules/app"
    component                 =  "backend"
    env                     =  var.env
