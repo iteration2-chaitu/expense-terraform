0a98334d5c259534294cf4f10bdb2cf902e12d01
chaitu <chaitus.ch09@gmail.com>
2024-10-11 22:20:28 -0700

    correcting the spelling mistake ..try doing creating instance via terraform

--- main.tf
+++ main.tf
@@ -18,15 +18,15 @@
 #   ssh_password            = var.ssh_password
 #   zone_id                 = var.zone_id
 # }
-# module "mysql" {
-#   source                  = "./modules/app"
-#   component                 =  "mysql"
-#   env                     =  var.env
-#   instance_type           = var.instance_type
-#   ssh_user                = var.ssh_user
-#   ssh_password            = var.ssh_password
-#   zone_id                 = var.zone_id
-# }
+ module "mysql" {
+   source                  = "./modules/app"
+   component                 =  "mysql"
+   env                     =  var.env
+   instance_type           = var.instance_type
+   ssh_user                = var.ssh_user
+   ssh_password            = var.ssh_password
+   zone_id                 = var.zone_id
+ }
 
 
 # module "backend" {
