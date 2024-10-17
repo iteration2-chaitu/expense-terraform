154cbd9ef2c9088caabca8aef723d6aef053b73f
chaitu <chaitus.ch09@gmail.com>
2024-10-17 14:29:40 -0700

    setting the vault_token variable

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -2,6 +2,6 @@
 env = "dev"
 instance_type = "t3.small"
 component = "frontend"
-ssh_user = "ec2-user"
-ssh_password = "DevOps321"
+#ssh_user = "ec2-user"
+#ssh_password = "DevOps321"
 zone_id = "Z1029901SH2BJPKJS7Q3"
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "env" {}
 variable "instance_type" {}
 variable "component"{}
-variable "ssh_user"{}
-variable "ssh_password"{}
+#variable "ssh_user"{}
+#variable "ssh_password"{}
 variable "zone_id" {}
 variable "vault_token"{}
