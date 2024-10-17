1aad8f1f5bcae79eaf2183f9da397a084d6e8e78
chaitu <chaitus.ch09@gmail.com>
2024-10-17 14:58:19 -0700

    setting the vault_token variable

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "vault" {
-  address = "https://vault.chaitus.shop:8200"
+  address = "https://vault-internal.chaitus.shop:8200"
   skip_tls_verify = true
   token           = var.vault_token
 }
