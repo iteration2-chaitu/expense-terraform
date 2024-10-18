00b2d6a9b126a2c7c3074726cc63990a45b7810e
chaitu <chaitus.ch09@gmail.com>
2024-10-18 11:48:34 -0700

    update the project for push + vault secrets integration with ansible and terraform

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -39,9 +39,9 @@ resource "null_resource" "ansible"{
     inline = [
 #      "sudo dnf install nginx -y",
 #      "sudo systemctl start nginx"
-      "sudo pip3.11 install ansible",
+      "sudo pip3.11 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/iteration2-chaitu/expense-ansible.git get-secrets.yml -e env=${var.env}  -e role_name=${var.component} -e vault_token= ${var.vault_token}",
-      "ansible-pull -i localhost, -U https://github.com/iteration2-chaitu/expense-ansible.git expense-pipeline.yml -e env=${var.env}  -e role_name=${var.component} -e @common.json -e @${var.component}.json"
+      "ansible-pull -i localhost, -U https://github.com/iteration2-chaitu/expense-ansible.git expense-pipeline.yml -e env=${var.env}  -e role_name=${var.component} -e @secrets.json -e @app.json"
 
     ]
   }
