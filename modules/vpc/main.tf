02968ae3fa05cb36f3b7c4e6711cac609f4b8ad4
chaitu <chaitus.ch09@gmail.com>
2024-10-28 11:04:45 -0700

    adding new vault_token

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,56 +1,60 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr_block
-#  "10.0.0.0/16"
+#resource "aws_vpc" "main" {
+#  cidr_block = var.vpc_cidr_block
+##  "10.0.0.0/16"
+#
+#  tags = {
+#    Name = "${var.env}-vpc"
+#  }
+#}
+#
+## this need to be removed as we r creating more subnets
+#resource "aws_subnet" "main" {
+#  vpc_id     = aws_vpc.main.id
+#  cidr_block = var.subnet_cidr_block
+#
+#  tags = {
+#    Name = "${var.env}-subnet"
+#  }
+#}
+#
+#resource "aws_vpc_peering_connection" "main" {
+##  peer_owner_id = var.peer_owner_id ..not mandatory
+#  peer_vpc_id   = var.default_vpc_id
+#  vpc_id        = aws_vpc.main.id
+#  auto_accept = true
+#
+#  tags = {
+#    Name = "${var.env}-vpc-to-default-vpc"
+#  }
+#}
+#
+#resource "aws_route"  "main" {
+#  route_table_id = aws_vpc.main.default_route_table_id
+#  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+#  destination_cidr_block    = var.default_vpc_cidr
+#
+#}
+#
+#resource "aws_route"  "default-vpc" {
+#  route_table_id =  var.default_route_table_id
+#  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+#  destination_cidr_block    = var.vpc_cidr_block
+#
+#}
+#
+#resource "aws_subnet" "frontend" {
+#  count = length(var.frontend_subnets)
+#  vpc_id = aws_vpc.main.id
+#  cidr_block =  var.frontend_subnets[count.index]
+#  availability_zone =  var.availability_zones[count.index]
+#  tags = {
+#    Name = "${var.env}-frontend-subnet-{count.index}"
+#  }
+#}
 
-  tags = {
-    Name = "${var.env}-vpc"
-  }
-}
 
-# this need to be removed as we r creating more subnets
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet_cidr_block
 
-  tags = {
-    Name = "${var.env}-subnet"
-  }
-}
 
-resource "aws_vpc_peering_connection" "main" {
-#  peer_owner_id = var.peer_owner_id ..not mandatory
-  peer_vpc_id   = var.default_vpc_id
-  vpc_id        = aws_vpc.main.id
-  auto_accept = true
-
-  tags = {
-    Name = "${var.env}-vpc-to-default-vpc"
-  }
-}
-
-resource "aws_route"  "main" {
-  route_table_id = aws_vpc.main.default_route_table_id
-  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-  destination_cidr_block    = var.default_vpc_cidr
-
-}
-
-resource "aws_route"  "default-vpc" {
-  route_table_id =  var.default_route_table_id
-  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-  destination_cidr_block    = var.vpc_cidr_block
-
-}
-
-resource "aws_subnet" "frontend" {
-  count = length(var.frontend_subnets)
-  vpc_id = aws_vpc.main.id
-  cidr_block =  var.frontend_subnets[count.index]
-  availability_zone =  var.availability_zones[count.index]
-  tags = {
-    Name = "${var.env}-frontend-subnet-{count.index}"
-  }
-}
 #resource "aws_subnet" "backend" {
 #  vpc_id = aws_vpc.main.id
 #}
