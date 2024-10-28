4b558dadf24e8838bf0de9bfe51e621dcc804ac1
chaitu <chaitus.ch09@gmail.com>
2024-10-28 14:11:37 -0700

    testing the vpc +subnets creation

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -49,31 +49,31 @@ resource "aws_route53_record" "instance"{
   ttl = 30
 }
 
-resource "null_resource" "ansible"{
-
-  triggers = {
-    always_run = "${timestamp()}"
-  }
-  provisioner "remote-exec" {
-
-    connection {
-      type     = "ssh"
-      //user     = var.ssh_user   #"ec2-user"
-      user     = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_user
-      //password = var.ssh_password   #"DevOps321"
-      password = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_password
-      host     = aws_instance.instance.public_ip
-    }
-
-    inline = [
-#      "sudo dnf install nginx -y",
-#      "sudo systemctl start nginx"
-      "sudo pip3.11 install ansible hvac",
-      "ansible-pull -i localhost, -U https://github.com/iteration2-chaitu/expense-ansible.git get-secrets.yml -e env=${var.env}  -e role_name=${var.component} -e vault_token=${var.vault_token}",
-      "ansible-pull -i localhost, -U https://github.com/iteration2-chaitu/expense-ansible.git expense-pipeline.yml -e env=${var.env}  -e role_name=${var.component} -e @~/secrets.json -e @~/app.json",
-      "rm -rf ~/secrets.json ~/app.json"
-
-    ]
-  }
-
-}
+#resource "null_resource" "ansible"{
+#
+#  triggers = {
+#    always_run = "${timestamp()}"
+#  }
+#  provisioner "remote-exec" {
+#
+#    connection {
+#      type     = "ssh"
+#      //user     = var.ssh_user   #"ec2-user"
+#      user     = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_user
+#      //password = var.ssh_password   #"DevOps321"
+#      password = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_password
+#      host     = aws_instance.instance.public_ip
+#    }
+#
+#    inline = [
+##      "sudo dnf install nginx -y",
+##      "sudo systemctl start nginx"
+#      "sudo pip3.11 install ansible hvac",
+#      "ansible-pull -i localhost, -U https://github.com/iteration2-chaitu/expense-ansible.git get-secrets.yml -e env=${var.env}  -e role_name=${var.component} -e vault_token=${var.vault_token}",
+#      "ansible-pull -i localhost, -U https://github.com/iteration2-chaitu/expense-ansible.git expense-pipeline.yml -e env=${var.env}  -e role_name=${var.component} -e @~/secrets.json -e @~/app.json",
+#      "rm -rf ~/secrets.json ~/app.json"
+#
+#    ]
+#  }
+#
+#}
