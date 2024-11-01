2b2f29facd1768af65e1a91d6993f7d2fdc7a0e9
chaitu <chaitus.ch09@gmail.com>
2024-11-01 13:35:24 -0700

    change the aws_route53 record type

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -71,7 +71,7 @@ resource "aws_route53_record" "load_balancer"{
   name = "${var.component}-${var.env}"
   type = "CNAME"
   zone_id = var.zone_id
-  records = [aws_lb.main[0].dns_name]
+  records = [aws_instance.instance.private_ip]
   ttl = 30
 }
 
