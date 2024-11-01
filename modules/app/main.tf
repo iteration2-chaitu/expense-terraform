9298bb534a35d4d6c3642c81431a41fb4b7645e9
chaitu <chaitus.ch09@gmail.com>
2024-11-01 13:59:56 -0700

    fix the error

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -72,7 +72,7 @@ resource "aws_route53_record" "load_balancer"{
   type = "CNAME"
   zone_id = var.zone_id
 #  records = [aws_lb.main[0].dns_name]
-  records = var.lb_needed ? [aws_lb.main[0].dns_name] : []
+  records  = var.lb_needed && length(aws_lb.main) > 0 ? [aws_lb.main[0].dns_name] : []
   ttl = 30
 }
 
