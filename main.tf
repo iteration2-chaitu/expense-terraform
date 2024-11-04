238b4478793feea7a4dbe65621d5ab2c68e7e638
chaitu <chaitus.ch09@gmail.com>
2024-11-04 14:22:35 -0800

    fix the typo

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@
    allocated_storage = 20
    component         = "rds"
    engine            = "mysql"
-   engine_version    = "8'0.36"
+   engine_version    = "8.0.36"
    env               = var.env
    family            = "mysql8.0"
    instance_class    = "db.t3.micro"
