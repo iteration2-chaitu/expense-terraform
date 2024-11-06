733f0c8f94da0feb1e2ac86b6d6db9296ca63baa
chaitu <chaitus.ch09@gmail.com>
2024-11-06 15:21:40 -0800

    add userdata to ASG

--- modules/app-asg/userdata.sh
+++ modules/app-asg/userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-       pip3.11 install ansible hvac &>>/opt/ansible.log
+     sudo pip3.11 install ansible hvac &>>/opt/ansible.log
     ansible-pull -i localhost, -U https://github.com/iteration2-chaitu/expense-ansible.git get-secrets.yml -e env=${env}  -e role_name=${component} -e vault_token=${vault_token}&>>/opt/ansible.log
      ansible-pull -i localhost, -U https://github.com/iteration2-chaitu/expense-ansible.git expense-pipeline.yml -e env=${env}  -e role_name=${component} -e @~/secrets.json&>>/opt/ansible.log
