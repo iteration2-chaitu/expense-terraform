4dde74fe08ae2892b448946a5a3746ba2e7565ee
chaitu <chaitus.ch09@gmail.com>
2024-11-06 13:57:48 -0800

    fixing the error

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -18,7 +18,8 @@ resource "aws_db_instance" "main" {
   storage_encrypted      = true  # this is true if we uses KMS
   kms_key_id             = var.kms_key_id
   backup_retention_period = 35
-  backup_window = "09:46-10:15"
+  backup_window =  "09:46-10:16"
+"
 }
 
 resource "aws_db_parameter_group" "main" {
