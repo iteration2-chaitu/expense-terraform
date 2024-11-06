f782a0976efde84a7faee5a5ed3b0b49b9895b64
chaitu <chaitus.ch09@gmail.com>
2024-11-06 13:29:25 -0800

    fixing the typo

--- modules/app-asg/main.tf
+++ modules/app-asg/main.tf
@@ -10,7 +10,7 @@ resource "aws_autoscaling_group" "main" {
   desired_capacity    = var.min_capacity
   max_size            = var.max_capacity
   min_size            = var.min_capacity
-  vpc_zone_identifier = [var.subnets]
+  vpc_zone_identifier = var.subnets
   target_group_arns   = [aws_lb_target_group.main.arn]
 
   launch_template {
