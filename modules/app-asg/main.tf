e77cb48fc3232a5049cddcde0eda6842928370e2
chaitu <chaitus.ch09@gmail.com>
2024-11-07 13:27:35 -0800

    Adding load balancer to ASG

--- modules/app-asg/main.tf
+++ modules/app-asg/main.tf
@@ -116,12 +116,12 @@ resource "aws_lb" "main" {
 
 
 resource "aws_security_group" "load-balancer" {
-  name        = "${var.component}-${var.env}-sg"
-  description = "${var.component}-${var.env}-sg"
+  name        = "${var.component}-${var.env}-sg-asg"
+  description = "${var.component}-${var.env}-sg-asg"
   vpc_id      = var.vpc_id           #aws_vpc.main.id
 
   tags = {
-    Name = "${var.component}-${var.env}-sg"
+    Name = "${var.component}-${var.env}-sg-asg"
   }
   ingress {
     from_port        = var.app_port   #0
